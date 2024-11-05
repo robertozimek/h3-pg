01b1f460da4b36a70a9e4f1a9f306f10f2692e97
Zacharias Knudsen <z@chari.as>
2024-11-05 11:28:36 +0100

    Trying to fix windows test

--- h3/test/sql/type.sql
+++ h3/test/sql/type.sql
@@ -26,14 +26,31 @@ SELECT :hexagon = :asbigint::h3index;
 --
 -- TEST binary io
 --
-CREATE TEMPORARY TABLE h3_test_binary_send (hex h3index PRIMARY KEY);
-CREATE TEMPORARY TABLE h3_test_binary_recv (hex h3index PRIMARY KEY);
-INSERT INTO h3_test_binary_send (hex) SELECT * from h3_get_res_0_cells();
-COPY h3_test_binary_send TO '/tmp/h3_test_binary.bin' (FORMAT binary);
-COPY h3_test_binary_recv FROM '/tmp/h3_test_binary.bin' (FORMAT binary);
+CREATE FUNCTION h3_test_binary_io() RETURNS boolean LANGUAGE PLPGSQL AS $$
+    DECLARE
+        result boolean;
+    BEGIN
+        CREATE TEMPORARY TABLE h3_test_binary_send (hex h3index PRIMARY KEY);
+        CREATE TEMPORARY TABLE h3_test_binary_recv (hex h3index PRIMARY KEY);
+        INSERT INTO h3_test_binary_send (hex) SELECT * from h3_get_res_0_cells();
 
--- same data after re-import
-SELECT array_agg(hex) is null FROM (
-	SELECT hex FROM h3_test_binary_send
-	EXCEPT SELECT hex FROM h3_test_binary_recv
-) q;
+        -- base temp dir on platform
+        IF (SELECT setting = 'windows' FROM pg_catalog.pg_file_settings WHERE name = 'dynamic_shared_memory_type')
+        THEN
+            COPY h3_test_binary_send TO '%localappdata%\Temp\h3_test_binary.bin' (FORMAT binary);
+            COPY h3_test_binary_recv FROM '%localappdata%\Temp\h3_test_binary.bin' (FORMAT binary);
+        ELSE
+            COPY h3_test_binary_send TO '/tmp/h3_test_binary.bin' (FORMAT binary);
+            COPY h3_test_binary_recv FROM '/tmp/h3_test_binary.bin' (FORMAT binary);
+        END IF;
+
+        -- same data after re-import
+        SELECT array_agg(hex) is null INTO result FROM (
+            SELECT hex FROM h3_test_binary_send
+            EXCEPT SELECT hex FROM h3_test_binary_recv
+        ) q;
+
+        RETURN result;
+    END
+$$;
+SELECT h3_test_binary_io();
