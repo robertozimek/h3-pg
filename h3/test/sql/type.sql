75839a4af81552a8e76bb4a2f9c8df99608f5129
Zacharias Knudsen <z@chari.as>
2024-11-05 11:54:28 +0100

    Add CHANGELOG entry, maybe fix windows test

--- h3/test/sql/type.sql
+++ h3/test/sql/type.sql
@@ -26,31 +26,16 @@ SELECT :hexagon = :asbigint::h3index;
 --
 -- TEST binary io
 --
-CREATE FUNCTION h3_test_binary_io() RETURNS boolean LANGUAGE PLPGSQL AS $$
-    DECLARE
-        result boolean;
-    BEGIN
-        CREATE TEMPORARY TABLE h3_test_binary_send (hex h3index PRIMARY KEY);
-        CREATE TEMPORARY TABLE h3_test_binary_recv (hex h3index PRIMARY KEY);
-        INSERT INTO h3_test_binary_send (hex) SELECT * from h3_get_res_0_cells();
+CREATE TEMPORARY TABLE h3_test_binary_send (hex h3index PRIMARY KEY);
+CREATE TEMPORARY TABLE h3_test_binary_recv (hex h3index PRIMARY KEY);
+INSERT INTO h3_test_binary_send (hex) SELECT * from h3_get_res_0_cells();
 
-        -- base temp dir on platform
-        IF (SELECT setting = 'windows' FROM pg_catalog.pg_file_settings WHERE name = 'dynamic_shared_memory_type')
-        THEN
-            COPY h3_test_binary_send TO 'C:\Windows\Temp\h3_test_binary.bin' (FORMAT binary);
-            COPY h3_test_binary_recv FROM 'C:\Windows\Temp\h3_test_binary.bin' (FORMAT binary);
-        ELSE
-            COPY h3_test_binary_send TO '/tmp/h3_test_binary.bin' (FORMAT binary);
-            COPY h3_test_binary_recv FROM '/tmp/h3_test_binary.bin' (FORMAT binary);
-        END IF;
+-- we need to use \copy instead of SQL COPY in order to have relative path
+\copy h3_test_binary_send TO 'h3_test_binary.bin' (FORMAT binary)
+\copy h3_test_binary_recv FROM 'h3_test_binary.bin' (FORMAT binary)
 
-        -- same data after re-import
-        SELECT array_agg(hex) is null INTO result FROM (
-            SELECT hex FROM h3_test_binary_send
-            EXCEPT SELECT hex FROM h3_test_binary_recv
-        ) q;
-
-        RETURN result;
-    END
-$$;
-SELECT h3_test_binary_io();
+-- make sure re-imported data matches original data
+SELECT array_agg(hex) is null FROM (
+    SELECT hex FROM h3_test_binary_send
+    EXCEPT SELECT hex FROM h3_test_binary_recv
+) q;
